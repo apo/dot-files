6c14be2a6b27f071b21ca6204f08d356a4c4aa80
apo <apo@users.noreply.github.com>
2015-05-20 12:53:55 +0800

    add web-mode

--- dot-emacs.d/init.el
+++ dot-emacs.d/init.el
@@ -47,3 +47,23 @@
 (setq erlang-root-dir "/usr/local/Cellar/erlang/17.4.1")
 (setq exec-path (cons "/usr/local/Cellar/erlang/17.4.1/bin" exec-path))
 (require 'erlang-start)
+
+;; web-mode
+;; http://web-mode.org
+(add-to-list 'load-path "~/.emacs.d/plugins/web-mode")
+(require 'web-mode)
+(add-to-list 'auto-mode-alist '("\\.phtml\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.tpl\\.php\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.[agj]sp\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.as[cp]x\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.erb\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.mustache\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.djhtml\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.html?\\'" . web-mode))
+;
+;(add-to-list 'auto-mode-alist '("\\.api\\'" . web-mode))
+;(add-to-list 'auto-mode-alist '("/some/react/path/*\\.js[x]?\\'" . web-mode))
+;(setq web-mode-content-types-alist
+;      '(("json" . "/some/path/*\\.api\\'")
+;	("xml"  . "/other/path/*\\.api\\'")
+;	("jsx"  . "/some/react/path/*\\.js[x]?\\'")))
